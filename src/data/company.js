0863a63233e01b170fa1f452d35b444b43322b10
wendsaw <sawadogo.edgard@gmail.com>
2026-03-19 16:02:25 +0000

    @latest

--- src/data/company.js
+++ src/data/company.js
@@ -11,19 +11,19 @@ export const CONTACTS = {
   pdg: {
     title: "PDG",
     name: "Wendpingré Edgard Sawadogo",
-    phones: ["+22665823105", "+22671082315"],
+    phones: ["+22625452315", "+22671082315"],
     email: "wend@mjsconstruction.org",
   },
   dt: {
     title: "Directeur Technique",
     name: "William Sawadogo",
-    phones: ["+22676252735", "+22672221267"],
+    phones: ["+22676252735", "+22625452315"],
     email: "contact@mjsconstruction.org",
   },
   daf: {
     title: "Directrice Administrative et Financière",
     name: "Madame Ouattara Maria Edwige",
-    phones: ["+22654699182", "+22671939180"],
+    phones: ["+22654699182", "+22625452315"],
     email: "contact@mjsconstruction.org",
   },
 };
